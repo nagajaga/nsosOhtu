3c931d2dcdf880d2025e495e047b27ae3c09b98f
Olli Määttä <ollimaatta@gmail.com>
2019-12-06 19:19:47 +0200

    init database manager

---
+++ src/main/java/ohtu/db/DatabaseManager.java
@@ -0,0 +1,70 @@
+package ohtu.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * A singleton class for handling database settings.
+ */
+public class DatabaseManager {
+
+    private static DatabaseManager instance;
+    private String path;
+    private String username;
+    private String password;
+    private int charLimit;
+
+    private DatabaseManager() {
+        charLimit = 10;
+    }
+
+    public static DatabaseManager getInstance() {
+        if (instance == null) {
+            instance = new DatabaseManager();
+        }
+        return instance;
+    }
+
+    /**
+     * Sets up the database variables. Run this before openConnection.
+     *
+     * @param path
+     * @param username
+     * @param password
+     */
+    public void setup(String path, String username, String password) {
+        this.path = path;
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Sets up the database.
+     *
+     * @throws SQLException
+     */
+    public void createTablesIfAbsent() throws SQLException {
+        Connection conn = openConnection();
+        conn.prepareStatement("create table if not exists Person(id integer primary key auto_increment,"
+                + " name varchar(" + charLimit + "));").executeUpdate();
+        conn.prepareStatement("create table if not exists Record(id integer primary key auto_increment,"
+                + " personId integer, score integer, difficulty integer,"
+                + " foreign key (personId) references Person(id));").executeUpdate();
+        conn.close();
+    }
+
+    public int getCharLimit() {
+        return charLimit;
+    }
+
+    /**
+     * Get a connection from DriverManager. Run setup before this.
+     *
+     * @return
+     * @throws SQLException
+     */
+    public Connection openConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection(path, username, password);
+        return conn;
+    }
+}

e605a768a3a347fff9d23ff8b88cde230401bc72
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2019-12-09 05:25:39 +0200

    Small changes to last commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         new ohtu.App(new ConsoleIO(), new FakeWorkDao()).run();
         /*DatabaseManager dbm = new DatabaseManager();
-        dbm.setup("jdbc:h2:.", "nsos", "snek");
+        dbm.setup("jdbc:h2:works.db", "nsos", "snek");
         new ohtu.App(new ConsoleIO(), new WorkDaoImpl(dbm)).run();*/
     }
 

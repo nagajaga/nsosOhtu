35380269d2e752fef4a50b7c794d43b0e7403f32
Olli Määttä <ollimaatta@gmail.com>
2019-12-09 14:44:03 +0200

    local changes to workdaoimpl, new constructor for work

--- src/main/java/ohtu/dao/impl/WorkDaoImpl.java
+++ src/main/java/ohtu/dao/impl/WorkDaoImpl.java
@@ -5,16 +5,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import ohtu.domain.Work;
 import ohtu.dao.WorkDao;
 import ohtu.db.DatabaseManager;
+import ohtu.domain.WorkType;
 
 public class WorkDaoImpl implements WorkDao {
 
     private DatabaseManager db;
+    private ArrayList<Work> works;
 
     public WorkDaoImpl(DatabaseManager db) {
         this.db = db;
@@ -66,7 +69,41 @@ public class WorkDaoImpl implements WorkDao {
 
     @Override
     public List<Work> list() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<Work> ret = new ArrayList<>();
+        try {
+            Connection conn = db.openConnection();
+            PreparedStatement s = conn.prepareStatement("select * from Work;");
+            ResultSet results = s.executeQuery();
+            while (results.next()) {
+                Work work = new Work(
+                        results.getInt("id"),
+                        results.getString("author"),
+                        results.getString("title"),
+                        results.getString("code"),
+                        results.getString("tags"),
+                        mapType(results.getString("type")),
+                        results.getBoolean("read"),
+                        results.getInt("pages"),
+                        results.getInt("current_page")
+                );
+                ret.add(work);
+            }
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println("Database exception: " + e.getMessage());
+        }
+        works.clear();
+        works.addAll(ret);
+        return ret;
+    }
+
+    private WorkType mapType(String str) {
+        for (WorkType value : WorkType.values()) {
+            if (value.toString().equalsIgnoreCase(str)) {
+                return value;
+            }
+        }
+        return null;
     }
 
     @Override

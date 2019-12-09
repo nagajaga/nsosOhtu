89153130b703b13899f85b212a60e09815b6c0f6
Kai Hartzell <kai.hartzell@gmail.com>
2019-12-09 13:51:35 +0200

    Added app.db to gitignore

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,24 @@
 package main;
 
-import ohtu.dao.fake.FakeWorkDao;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import ohtu.io.ConsoleIO;
+import ohtu.dao.fake.*;
+import ohtu.dao.impl.WorkDaoImpl;
+import ohtu.db.*;
 
 public class Main {
 
     public static void main(String[] args) {
-//        try {
-//            DatabaseManager dbm = new DatabaseManager();
-//            dbm.setup("jdbc:sqlite:resources/app.db", "nsos", "snek");
-//            dbm.createTablesIfAbsent();
-//            new ohtu.App(new ConsoleIO(), new WorkDaoImpl(dbm)).run();
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        try {
+            DatabaseManager dbm = new DatabaseManager();
+            dbm.setup("jdbc:sqlite:app.db", "nsos", "snek");
+            dbm.createTablesIfAbsent();
+            new ohtu.App(new ConsoleIO(), new WorkDaoImpl(dbm)).run();
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
         new ohtu.App(new ConsoleIO(), new FakeWorkDao()).run();
     }
 

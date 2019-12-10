1f1124b45a807c27090325bbf6ebe1b92356543d
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2019-12-10 03:25:05 +0200

    A current page in book can now be saved

--- src/main/java/ohtu/App.java
+++ src/main/java/ohtu/App.java
@@ -24,7 +24,7 @@ public class App {
         io.println("Hello!");
 
         while (true) {
-            io.print("Add/List/Search/Edit/Delete/Quit (A/L/S/E/D/Q): ");
+            io.print("Add / List / Search / Update current page / Edit / Delete / Quit (A/L/S/U/E/D/Q): ");
             String input = io.nextLine();
             if (input.equalsIgnoreCase("Q")) {
                 break;
@@ -38,6 +38,8 @@ public class App {
                 handleEditing();
             } else if (input.equalsIgnoreCase("D")) {
                 handleDeleting();
+            } else if (input.equalsIgnoreCase("U")) {
+                handleUpdatingCurrentPage();
             }
         }
         io.println("\nGoodbye!");
@@ -188,6 +190,34 @@ public class App {
                 io.println("Unexpected error\n");
             }
         } else if (!dao.list().isEmpty()) {
+            io.println("Item not found\n");
+        }
+    }
+    
+    private void handleUpdatingCurrentPage() {
+        int id = askId("update the current page for");
+        if (id >= 0 && dao.read(id) != null) {
+            Work copy = dao.read(id);
+            if (copy == null) {
+                io.println("Invalid id\n");
+                return;
+            } else if (copy.getType() != WorkType.BOOK) {
+                io.println("That is not a book :)\n");
+                return;
+            }
+            int updateWith = askCurrentPage(copy);
+            if (updateWith != copy.getCurrentPage()) {
+                copy.setCurrentPage(updateWith);
+                if (dao.update(copy) == null) {
+                    io.println("Unexpected error\n");
+                    return;
+                }
+                io.println("Current page successfully updated\n");
+            } else {
+                io.println("Failed updating the current page\n");
+            }
+            
+        } else {
             io.println("Item not found\n");
         }
     }
@@ -345,6 +375,23 @@ public class App {
             pages = Integer.parseInt(input);
         }
         return pages;
+    }
+    
+    private Integer askCurrentPage(Work copy) {
+        int page = copy.getCurrentPage();
+        io.print("Last time you were on page " + copy.getCurrentPage() + "/" + copy.getPages() + ". Leaving the field empty cancels. Enter a new page number: ");
+        String input = io.nextLine();
+        Pattern pattern = Pattern.compile("\\d+");
+        if (input.equalsIgnoreCase("")) {
+            page = copy.getCurrentPage();
+        } else if (pattern.matcher(input).matches()) {
+            page = Integer.parseInt(input);
+            if (page > copy.getPages() && page >= 0) {
+                io.println("Invalid page number");
+                page = copy.getCurrentPage();
+            }
+        }
+        return page;
     }
 
     private void createWork(boolean editing, String author, String title, String tags, WorkType type, String url, int pages, boolean read, int id) {
